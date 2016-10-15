c4dc4d12669219e3c63dbffa2c07803dace5f02a
Samuel Holland <samuel@sholland.org>
2016-10-15 07:53:37 -0500

    Cleanup on RandomStack

--- app/src/main/java/xyz/smaeul/xisalone/RandomStack.java
+++ app/src/main/java/xyz/smaeul/xisalone/RandomStack.java
@@ -2,81 +2,42 @@ package xyz.smaeul.xisalone;
 
 import java.util.Random;
 import java.util.Stack;
-import xyz.smaeul.xisalone.expression.*;
+
+import xyz.smaeul.xisalone.expression.Term;
 
 /**
  * Created by Sai Kovuri on 10/14/2016.
+ * Refactored twice on 10/15/2016.
  */
 
 public class RandomStack {
+    private static int MAX = 20;
 
-    Stack<Term> values;
-    Stack<Operator> operators;
+    private Random rand;
+    private Stack<Term> values;
+    private Stack<Operator> operators;
 
-    public RandomStack(int n){
+    public RandomStack(int n) {
+        rand = new Random();
+        operators = new Stack<>();
+        values = new Stack<>();
 
-        setOperators(n);
-        setValues(n);
-    }
-
-    private void setValues(int n){
-
-        values = new Stack<Term>();
-        Random rand = new Random();
-
-        for(int i = 0; i < n; i++){
-
-            int num = rand.nextInt() % 20;
-            if(num >= 0){
-
+        for (int i = 0; i < n; i++) {
+            int num = rand.nextInt(MAX);
+            if (num >= 0) {
                 values.push(new Term(num, 0));
-            }else{
-
+            } else {
                 values.push(new Term(1, 1));
             }
+            operators.push(Operator.values()[rand.nextInt(Operator.values().length)]);
         }
     }
 
-    private void setOperators(int n){
-
-        operators = new Stack<Operator>();
-        Random rand = new Random();
-
-        for(int i = 0; i < n; i++){
-
-            int num = rand.nextInt() % 4;
-
-            switch(num){
-
-                case 0:
-
-                    operators.push(Operator.ADD);
-                    break;
-
-                case 1:
-
-                    operators.push(Operator.DIVIDE);
-                    break;
-
-                case 2:
-
-                    operators.push(Operator.SUBTRACT);
-                    break;
-
-                case 3:
-                    operators.push(Operator.MULTIPLY);
-                    break;
-            }
-        }
-    }
-
-    public Stack<Term> getValues(){
-
+    public Stack<Term> getValues() {
         return values;
     }
 
-    public Stack<Operator> getOperators(){
-
+    public Stack<Operator> getOperators() {
         return operators;
     }
 }
