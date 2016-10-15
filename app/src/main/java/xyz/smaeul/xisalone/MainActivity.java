2dcc1921b022ef4eb3fe244ffc8c7b0818a20000
Christopher Wong <cwong164@gmail.com>
2016-10-15 02:44:15 -0500

    Added framework for up/down/left/right controls

--- app/src/main/java/xyz/smaeul/xisalone/MainActivity.java
+++ app/src/main/java/xyz/smaeul/xisalone/MainActivity.java
@@ -1,7 +1,9 @@
 package xyz.smaeul.xisalone;
 
 import android.os.Bundle;
+import android.support.v4.view.MotionEventCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -10,12 +12,87 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final int steps = 4;
+    private class Coordinates {
+        private float x;
+        private float y;
+    }
+
+    final Coordinates savedCoordinates = new Coordinates();
+
+    // Number of operations to perform that the user must undo
+    private static final int steps = 4;
+    // Radius size for circle in pixels: motion outside of this circle is considered input
+    private static final float radius = 100f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView text = (TextView) findViewById(R.id.operation);
+        final View view = findViewById(R.id.activity_main);
+
+        view.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int action = MotionEventCompat.getActionMasked(event);
+                float currentX = event.getAxisValue(0);
+                float currentY = event.getAxisValue(1);
+
+                switch (action) {
+                    // Set initial x and y coordinate when screen is pressed
+                    case (MotionEvent.ACTION_DOWN):
+                        savedCoordinates.x = currentX;
+                        savedCoordinates.y = currentY;
+                        return true;
+
+                    // Track input while moving
+                    case (MotionEvent.ACTION_MOVE):
+                        // If outside the circle
+                        if (Math.pow((savedCoordinates.x - currentX), 2) + Math.pow((savedCoordinates.y - currentY), 2) > Math.pow(radius, 2)) {
+                            if (Math.abs(savedCoordinates.x - currentX) > Math.abs(savedCoordinates.y - currentY)) {
+                                if (savedCoordinates.x < currentX) {
+                                    text.setText("right");
+                                } else {
+                                    text.setText("left");
+                                }
+                            } else {
+                                if (savedCoordinates.y < currentY) {
+                                    text.setText("down");
+                                } else {
+                                    text.setText("up");
+                                }
+                            }
+                        }
+                        return true;
+
+                    // When finger is lifted
+                    case (MotionEvent.ACTION_UP):
+                        // If outside the circle
+                        if (Math.pow((savedCoordinates.x - currentX), 2) + Math.pow((savedCoordinates.y - currentY), 2) > Math.pow(radius, 2)) {
+                            if (Math.abs(savedCoordinates.x - currentX) > Math.abs(savedCoordinates.y - currentY)) {
+                                if (savedCoordinates.x < currentX) {
+                                    text.setText("right");
+                                } else {
+                                    text.setText("left");
+                                }
+                            } else {
+                                if (savedCoordinates.y < currentY) {
+                                    text.setText("down");
+                                } else {
+                                    text.setText("up");
+                                }
+                            }
+                        } else {
+                            text.setText("canceled");
+                        }
+                        return true;
+
+                    default:
+                        return false;
+                }
+            }
+        });
 
         startGame();
     }
