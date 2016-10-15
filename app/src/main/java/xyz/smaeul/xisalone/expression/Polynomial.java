59c35639882f1ef45f71304c6bdbe3d8d435f01e
Christopher Wong <cwong164@gmail.com>
2016-10-15 07:00:21 -0500

    Added toHTML method

--- app/src/main/java/xyz/smaeul/xisalone/expression/Polynomial.java
+++ app/src/main/java/xyz/smaeul/xisalone/expression/Polynomial.java
@@ -49,4 +49,79 @@ public class Polynomial {
     public void subtract(Term t) {
         add(new Term(-t.coefficient, t.exponent));
     }
+
+    public String toHTML() {
+        // Array for term index sorted in reverse order by exponent
+        int listSize = terms.size();
+        int[] list = new int[listSize];
+        for(int i = 0; i < listSize; i++){
+            list[i] = i;
+        }
+        // Sort by exponent; Bubble sort
+        boolean listChanged = true;
+        while(listChanged == true){
+            listChanged = false;
+            for(int i = 0; i < (listSize - 1); i++){
+                if((terms.get(list[i])).getExponent() < (terms.get(list[i + 1])).getExponent()){
+                    listChanged = true;
+                    int temp = list[i];
+                    list[i] = list[i + 1];
+                    list[i + 1] = temp;
+                }
+            }
+        }
+        // Create HTML
+        String output = new String();
+        for(int i = 0; i < listSize; i++){
+            if(terms.get(list[i]).getCoefficient() > 0 && i == 0){
+                output = output.concat("" + terms.get(list[i]).getCoefficient());
+                if(terms.get(list[i]).getExponent() > 0) {
+                    output = output.concat("x");
+                }
+                if(terms.get(list[i]).getExponent() > 1) {
+                    output = output.concat("<sup>" + terms.get(list[i]).getExponent() + "</sup> ");
+                }
+                else {
+                    output = output.concat(" ");
+                }
+            }
+            else if(terms.get(list[i]).getCoefficient() < 0 && i == 0){
+                output = output.concat("-" + Math.abs(terms.get(list[i]).getCoefficient()));
+                if(terms.get(list[i]).getExponent() > 0) {
+                    output = output.concat("x");
+                }
+                if(terms.get(list[i]).getExponent() > 1) {
+                    output = output.concat("<sup>" + terms.get(list[i]).getExponent() + "</sup> ");
+                }
+                else {
+                    output = output.concat(" ");
+                }
+            }
+            else if(terms.get(list[i]).getCoefficient() > 0){
+                output = output.concat("+ " + terms.get(list[i]).getCoefficient());
+                if(terms.get(list[i]).getExponent() > 0) {
+                    output = output.concat("x");
+                }
+                if(terms.get(list[i]).getExponent() > 1) {
+                    output = output.concat("<sup>" + terms.get(list[i]).getExponent() + "</sup> ");
+                }
+                else {
+                    output = output.concat(" ");
+                }
+            }
+            else if(terms.get(list[i]).getCoefficient() < 0){
+                output = output.concat("- " + Math.abs(terms.get(list[i]).getCoefficient()));
+                if(terms.get(list[i]).getExponent() > 0) {
+                    output = output.concat("x");
+                }
+                if(terms.get(list[i]).getExponent() > 1) {
+                    output = output.concat("<sup>" + terms.get(list[i]).getExponent() + "</sup> ");
+                }
+                else {
+                    output = output.concat(" ");
+                }
+            }
+        }
+        return(output);
+    }
 }
